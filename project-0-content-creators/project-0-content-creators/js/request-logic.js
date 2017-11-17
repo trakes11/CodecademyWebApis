391b9dd30c7459367b4899d1da970e3b70369f64
Thomas Rakes <thomasa.rakes@libertymutual.com>
2017-11-16 19:26:57 -0500

    all tests are passing

--- project-0-content-creators/project-0-content-creators/js/request-logic.js
+++ project-0-content-creators/project-0-content-creators/js/request-logic.js
@@ -5,4 +5,22 @@
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  switch (extension){
+    case 'html':
+      return 'text/html';
+      break;
+    case 'css':
+      return 'text/css';
+      break;
+    case 'jpeg':
+      return 'image/jpeg';
+      break;
+    case 'jpg':
+      return 'image/jpeg';
+      break;
+    default:
+      return 'text/plain';
+      break;
+  }
 }
